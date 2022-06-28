808f40912711453ea504be28861a3ad352d7851d
BINGOO <54973413+Royalatomo@users.noreply.github.com>
2022-06-28 22:14:05 +0530

    Update ForgetPass.js

--- src/Pages/ForgetPass.js
+++ src/Pages/ForgetPass.js
@@ -66,7 +66,7 @@ const ForgetPass = () => {
       setErr("no password is given");
     }
     const req = await fetch(
-      `http://localhost:5000/api/user/forgot-pass/reset`,
+      `${API}/api/user/forgot-pass/reset`,
       {
         method: "POST",
         headers: {
